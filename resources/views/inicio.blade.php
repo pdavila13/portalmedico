a40689b5b973b22a0255c4e5581c51bba3dbb14c
Paolo Dávila <paolo.davilab@gmail.com>
2023-11-09 18:42:14 +0100

    Comprobar parametros si existen con @isset

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,4 +1,9 @@
 <html>
     <h1>Vista</h1>
     <p>hola {{ $name }} - {{ $surname }}</p>
+
+    <!-- Comprobar el parametro existe -->
+    @isset($phone)
+        <p>Teléfono: {{ $phone }}</p>
+    @endisset
 </html>
