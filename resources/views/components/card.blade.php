03030de534bb1ec7a8649d948a09f05fa58d01d5
Paolo Dávila <paolo.davilab@gmail.com>
2023-11-13 09:10:09 +0100

    Mas de una card con parametro  opcional

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,8 @@
 <div style="background-color: #{{ $bgcolor }}">
     <h2>{{ $paciente }}</h2>
+    @isset($fecha)
     {{ $fecha }}
+    @endisset
+    
     {{ $texto }}
 </div>
--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -11,5 +11,13 @@
             mollit anim id est laborum.
         </x-slot:texto>
     </x-card>
-    {{ $nombre }}
+
+    <x-card bgcolor="FFA0A0" :paciente="$nombre">
+        <x-slot:texto >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <strong>Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat nulla pariatur</strong>. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+            mollit anim id est laborum.
+        </x-slot:texto>
+    </x-card>
 @endsection
