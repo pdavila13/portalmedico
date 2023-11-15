8084855395739b0c2fb345311ddac2448fe553cc
Paolo Dávila <paolo.davilab@gmail.com>
2023-11-15 12:13:09 +0100

    Creando y mostrando botones como componente con colores

---
+++ app/View/Components/buttons.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Buttons extends Component
+{
+    public $botones = [];
+    public $bgcolor = "";
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($canEdit = 1) {
+    if ($canEdit == 1) {
+        $this->botones = ['Volver', 'Ver', 'Editar'];
+        $this->bgcolor="#A0FFA0";
+    } else {
+        $this->botones = ['Volver', 'Ver'];
+        $this->bgcolor="#FFFFA0";
+    }
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string {
+        return view('components.buttons');
+    }
+}
---
+++ resources/views/components/buttons.blade.php
@@ -0,0 +1,5 @@
+<div style="background-color: {{ $bgcolor }}">
+    @foreach ($botones as $boton)
+        <button> {{ $boton }}</button> 
+    @endforeach
+</div>
