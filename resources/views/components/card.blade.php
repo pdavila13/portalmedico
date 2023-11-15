437d6fc803dfad5fdc30444174711baf88ed4311
Paolo Davila <paolo.davilab@gmail.com>
2023-11-15 23:13:35 +0100

    Rectificando ancho a la card de creación

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,8 @@
+@isset($ancho)
+<div style="background-color: #{{ $bgcolor }}" class="flex-initial w-{{$ancho}} m-2 p-4">
+@else
 <div style="background-color: #{{ $bgcolor }}" class="flex-initial w-32 m-2 p-4">
+@endisset
     <h2>{{ $nombre }}</h2>
     @isset($fecha)
     {{ $fecha }}
--- resources/views/doctors/create.blade.php
+++ resources/views/doctors/create.blade.php
@@ -7,11 +7,11 @@
 @section('content')
     <h1>Cuatro Médico</h1>
     <div class="flex">
-        <x-card nombre="Nuevo Doctor" bgcolor="A0A0FF">
+        <x-card nombre="Nuevo Doctor" bgcolor="A0A0FF" ancho=80>
             <x-slot:texto>
             <label for="nombre">Nombre:</label>
             <input type="text" name="nombre" id="nombre" value="">
-
+            <br>&nbsp;<br>
             <label for="especialidad">Especialidad:</label>
             <input type="text" name="especialidad" id="especialidad" value="">
             </x-slot:texto>
