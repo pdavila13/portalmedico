32da04ca2743dca729f6145fada214039d1bfa27
Paolo Dávila <paolo.davilab@gmail.com>
2023-11-15 11:34:13 +0100

    Instalación y implementación de TailwindCSS

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css','resources/css/app.js'])
 </head>
 <body>
     <header>
