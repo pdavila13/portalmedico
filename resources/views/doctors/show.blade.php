0ee94b9adc34e43c39e96bd2758e0212ad4836fe
Paolo Davila <paolo.davilab@gmail.com>
2023-11-15 22:26:02 +0100

    Implementando metodo doctors.show con vistas

---
+++ resources/views/doctors/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('menu')
+    <x-menu/>
+@endsection
+
+@section('content')
+    <h1>Cuatro Médico</h1>
+    <div class="flex">
+        <x-card paciente="{{ $doctor[1] }} {{ $doctor[2] }}" bgcolor="A0A0FF" texto="{{ $doctor[3] }}">
+            <x-slot:botones>
+                <a href="{{ route('doctors.show', 0) }}" class="text-white bg-slate-300 m-4 p-1">Ver</a>
+            </x-slot:botones>    
+        </x-card>
+    </div>
+@endsection
