4312c05bd5ac9fec8e4512b8d5daf5a0fd7b3cee
Paolo Dávila <paolo.davilab@gmail.com>
2023-11-15 12:27:58 +0100

    Revert "Mas de una card con parametro opcional y codigo HTML"
    This reverts commit 4e74938187f847fd1f640baea1f4fdd2b049bc59.

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,7 @@
 <div style="background-color: #{{ $bgcolor }}">
     <h2>{{ $paciente }}</h2>
     @isset($fecha)
-        <h3>{{ $fecha }}</h3>
+    {{ $fecha }}
     @endisset
     
     {{ $texto }}
