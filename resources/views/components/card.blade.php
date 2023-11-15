0d76443ce309ae12b71c3861b7068ca859ede06a
Paolo Davila <paolo.davilab@gmail.com>
2023-11-15 21:54:58 +0100

    Aplicando estilo a la x-card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div style="background-color: #{{ $bgcolor }}">
+<div style="background-color: #{{ $bgcolor }}" class="flex-initial w-32 m-2 p-4">
     <h2>{{ $paciente }}</h2>
     @isset($fecha)
     {{ $fecha }}
--- resources/views/doctors/index.blade.php
+++ resources/views/doctors/index.blade.php
@@ -6,8 +6,9 @@
 
 @section('content')
     <h1>Cuatro Médico</h1>
-
-    @foreach ($doctors as $doctor)
-        <x-card paciente="{{ $doctor[1] }} {{ $doctor[2] }}" bgcolor="A0A0FF" texto="{{ $doctor[3] }}"/>
-    @endforeach
+    <div class="flex">
+        @foreach ($doctors as $doctor)
+            <x-card paciente="{{ $doctor[1] }} {{ $doctor[2] }}" bgcolor="A0A0FF" texto="{{ $doctor[3] }}"/>
+        @endforeach
+    </div>
 @endsection
