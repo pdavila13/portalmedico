91f1645b014aa5010239450814e34d81cb36eb64
Paolo Davila <paolo.davilab@gmail.com>
2023-11-20 21:42:43 +0100

    Validación para campos numericos usando validator!

--- app/Http/Requests/DoctorRequest.php
+++ app/Http/Requests/DoctorRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 
 class DoctorRequest extends FormRequest
@@ -21,19 +22,20 @@ class DoctorRequest extends FormRequest
      */
     public function rules(): array
     {
+        Validator::extend('par', function($attribute, $value, $parameters, $validator) {
+            return (($value % 2) == 0);
+        });
+
         return [
             'nombre' => 'required|min:3',
-            'especialidad' => 'required|max:200'
+            'especialidad' => 'required|par'
         ];
     }
 
     public function messages()
     {
         $mensajes = [
-            'nombre.required' => 'El nombre es obligatorio',
-            'especialidad' => [
-                'required' => 'La especialidad es obligatoria'
-            ]
+            'nombre.required' => 'El nombre es obligatorio'
         ];
         $mensajes = array_merge($mensajes, ['nombre.email' => 'El nombre no representa un email']);
 
