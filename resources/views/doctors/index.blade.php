a61048ca71ce4921006768077042224dc38a4fb0
Paolo Davila <paolo.davilab@gmail.com>
2023-11-20 20:53:55 +0100

    Mostrar texto: Doctor guardado correctamente al modificar un doctor y aplicar los cambios

--- resources/views/doctors/index.blade.php
+++ resources/views/doctors/index.blade.php
@@ -6,5 +6,12 @@
 
 @section('content')
     <h1>Cuatro Médico</h1>
+    
+    @if ($message = Session::get('message'))
+    <div>
+        {{ $message }}
+    </div>
+    @endif
+    
     <x-listado rutashow="doctors.show" :items="$doctors"/>
 @endsection
