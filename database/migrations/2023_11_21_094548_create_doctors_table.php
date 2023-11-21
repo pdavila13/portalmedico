a5139251a7fb23194475a4d7bc1530e8576ca51c
Paolo Dávila <paolo.davilab@gmail.com>
2023-11-21 11:56:47 +0100

    Agregando campos a la tabla doctors

--- database/migrations/2023_11_21_094548_create_doctors_table.php
+++ database/migrations/2023_11_21_094548_create_doctors_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('middleName');
+            $table->string('lastName')->nullable();
+            $table->string('speciality')->default('Medicina general');
+            $table->string('email',200)->nullable();
             $table->timestamps();
         });
     }
