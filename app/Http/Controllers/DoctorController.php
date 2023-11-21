91c399321fe5f1e6f151cc5f91e057a8936ecd3b
Paolo Dávila <paolo.davilab@gmail.com>
2023-11-21 14:47:55 +0100

    Se soluciona problemas con la importación de modelos

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doctor;
 use Illuminate\Http\Request;
 use App\Http\Requests\DoctorRequest;
 
--- database/migrations/2023_11_21_133449_create_doctor_patient_table.php
+++ database/migrations/2023_11_21_133449_create_doctor_patient_table.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Doctor;
+use App\Models\Patient;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
