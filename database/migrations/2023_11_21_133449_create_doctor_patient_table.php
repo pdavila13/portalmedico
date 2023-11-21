540d139d87960d3bb13a89de9379253c4a7d5156
Paolo Dávila <paolo.davilab@gmail.com>
2023-11-21 14:41:27 +0100

    Implementación de relación ManyToMany

--- database/migrations/2023_11_21_133449_create_doctor_patient_table.php
+++ database/migrations/2023_11_21_133449_create_doctor_patient_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('doctor_patient', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Doctor::class)->constrained();
+            $table->foreignIdFor(Patient::class)->constrained();
             $table->timestamps();
         });
     }
