4b8404b443743468ca6f07dd38b99fcf083f9163
Paolo Davila <paolo.davilab@gmail.com>
2023-11-21 21:03:48 +0100

    Implementación de Seeder manual

--- database/seeders/PatientSeeder.php
+++ database/seeders/PatientSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PatientSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class PatientSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('patients')->insert([
+            'name'=>'Paolo',
+            'middleName'=>'Davila',
+            'lastName'=>'Bazalar',
+            'email'=>'pdavila@exemple.com'
+        ]);
     }
 }
