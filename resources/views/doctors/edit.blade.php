06b134c378a96c9587f08ca33a1ca3c49b3c8fba
Paolo Davila <paolo.davilab@gmail.com>
2023-11-22 20:56:59 +0100

    Modificación de doctores

--- resources/views/doctors/edit.blade.php
+++ resources/views/doctors/edit.blade.php
@@ -7,21 +7,23 @@
 @section('content')
     <h1>Cuatro Médico</h1>
     <div class="flex">
-        <form method="POST" action="{{ route('doctors.update', $doctor[0]) }}">
+        <form method="POST" action="{{ route('doctors.update', $doctor->id) }}">
             @csrf
             @method('PUT')
-            <x-card nombre="Modificar Doctor" bgcolor="A0A0FF" ancho=80>
+            <x-card nombre="Modificar Doctor" bgcolor="A0A0FF" ancho="500">
                 <x-slot:texto>
-                <label for="nombre">Nombre:</label>
-                <input type="text" name="nombre" id="nombre" value="{{ $doctor[1] }}">
-                <br>&nbsp;<br>
-                <label for="especialidad">Especialidad:</label>
-                <input type="text" name="especialidad" id="especialidad" value="{{ $doctor[2] }}">
+                    <x-field name="name" label="Nombre" :value="$doctor->name"/>
+                    <x-field name="middleName" label="Primer Apellido" :value="$doctor->middleName"/>
+                    <x-field name="lastName" label="Segundo Apellido" :value="$doctor->lastName"/>
+                    <x-field name="speciality" label="Especialidad" :value="$doctor->speciality"/>
+                    <x-field name="phone" label="Teléfono" :value="$doctor->phone"/>
+                    <x-field name="dni" label="Nº Identificación" :value="$doctor->dni"/>
+                    <x-field name="email" label="Correo electrónico" :value="$doctor->email"/>
                 </x-slot:texto>
 
                 <x-slot:botones>
                     <button type="submit" class="text-white bg-slate-300 m-4 p-1">Guardar</button>
-                </x-slot:botones>    
+                </x-slot:botones>
             </x-card>
         </form>
     </div>
